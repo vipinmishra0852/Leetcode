e74ee33ef0a4b3616c1eaf6c2496520e9def0c23
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-04-01 18:35:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (57.50%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int count=0;
         for(int i=s.length()-1;i>=0;i--){
         if(s[i]==' ' && count==0) continue;
