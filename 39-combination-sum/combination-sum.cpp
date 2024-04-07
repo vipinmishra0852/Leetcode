a0008a89f435aa1893e26c37a132c203cd5f1f81
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-04-07 16:41:59 +0530

    Time: 3 ms (81.96%) | Memory: 12.4 MB (65.22%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+class Solution {
+public:
+    void combination(vector<vector<int>>& result, vector<int>& current, vector<int>& candidates, int target, int idx) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        
+        for (int i = idx; i < candidates.size(); i++) {
+            if (candidates[i] <= target) {
+                current.push_back(candidates[i]);
+                combination(result, current, candidates, target - candidates[i], i);
+                current.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        vector<vector<int>> result;
+        vector<int> current;
+        combination(result, current, candidates, target, 0);
+        return result;
+    }
+};
