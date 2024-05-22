e251565f42060a17dcb80ff6ca52434544e55f71
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-05-22 18:49:12 +0530

    Time: 8 ms (11.59%) | Memory: 49.2 MB (10.28%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        string str=countAndSay(n-1);
+        string ans="";
+        int freq=1;
+        char ch=str[0];
+        for(int i=1;i<str.length();i++){
+            char dh=str[i];
+            if(ch==dh)
+             {
+            freq++;
+            }
+            else{
+                ans=ans+(to_string(freq)+ch);
+                freq=1;
+                ch=dh;
+            }
+        }
+         ans=ans+(to_string(freq)+ch);
+         return ans;
+    }
+};
