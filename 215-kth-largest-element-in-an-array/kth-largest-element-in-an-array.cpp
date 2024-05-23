d09fbd6980cb45ebfe6050125b14feb18a785783
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-05-23 17:42:15 +0530

    Time: 2230 ms (5.01%) | Memory: 57.7 MB (87.87%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,8 +1,48 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
+    // Function to swap two elements
+    void swap(int &a, int &b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    // Partition function for Quickselect
+    int partition(vector<int> &arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low;
+
+        for (int j = low; j < high; ++j) {
+            if (arr[j] > pivot) { // Note the comparison here for kth largest
+                swap(arr[i], arr[j]);
+                ++i;
+            }
+        }
+        swap(arr[i], arr[high]);
+        return i;
+    }
+
+    // Quickselect function
+    int quickSelect(vector<int> &arr, int low, int high, int k) {
+        if (low <= high) {
+            int pi = partition(arr, low, high);
+
+            if (pi == k) {
+                return arr[pi];
+            } else if (pi < k) {
+                return quickSelect(arr, pi + 1, high, k);
+            } else {
+                return quickSelect(arr, low, pi - 1, k);
+            }
+        }
+        return -1; // This should never be reached if k is valid
+    }
+
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        return nums[n-k];
+        int n = nums.size();
+        return quickSelect(nums, 0, n - 1, k - 1);
     }
 };
