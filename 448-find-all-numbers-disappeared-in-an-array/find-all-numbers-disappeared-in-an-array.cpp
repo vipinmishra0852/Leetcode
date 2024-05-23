76d8ee26dc8c56869946849fb51b36ca3b639352
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-05-23 19:47:25 +0530

    Time: 18 ms (99.42%) | Memory: 37.4 MB (29.38%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        int n=nums.size();
+        vector<int>v(n+1,0);
+        for(int i=0;i<n;i++){
+            v[nums[i]]=1;
+        }
+        vector<int>ans;
+        for(int i=1;i<n+1;i++){
+            if(v[i]==0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
