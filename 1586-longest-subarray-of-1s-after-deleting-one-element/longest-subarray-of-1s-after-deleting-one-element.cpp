5b589ae22f9bca029b12663a85599928583cfe6a
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-05-25 15:03:01 +0530

    Time: 24 ms (99.20%) | Memory: 58.6 MB (75.77%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n=nums.size();
+        int i=0;
+        int j=0;
+        int k=1;
+        int length=INT_MIN;
+        int maxlen=INT_MIN;
+        int flips=0;
+        while(j<n){
+            if(nums[j]==1) j++;
+            else
+            {
+              if(flips<k) 
+              {
+                flips++;
+                j++;
+              }
+              else
+              {
+                length=j-i;
+                maxlen=max(maxlen,length);
+                while(nums[i]==1) {i++;}
+                i++;  
+                j++; 
+              }
+            }
+          
+        }
+          length=j-i;
+            maxlen=max(maxlen,length);
+        return maxlen-1;
+
+    }
+};
