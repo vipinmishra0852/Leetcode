9c46f1ffa2f8fba68014a7704deef39d6679edbe
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-05-31 22:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (74.83%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,40 +1,28 @@
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ios::sync_with_stdio(false);
-        cin.tie(NULL);
-        ListNode* a=list1;
-        ListNode* b=list2;
-        ListNode* c = new ListNode(100);
-        ListNode* tempC=c; 
-        while(a!=NULL && b!=NULL)
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+       ListNode* t=new ListNode(100);
+       ListNode* temp=t;
+
+       while(list1!=NULL && list2!=NULL)
+       {
+        if(list1->val<=list2->val)
         {
-            if(a->val<=b->val)
-            {
-              ListNode* e=new ListNode(a->val);
-              tempC->next=e;
-              tempC=e;
-              a=a->next;
-                
-            }
-            else
-            {
-                ListNode* e= new ListNode(b->val); 
-                tempC->next=e;
-                tempC=e;
-                b=b->next;
-            }
-        }
-        if(a==NULL)
-        {
-            tempC->next=b;
+            temp->next=list1;
+            list1=list1->next;
+            temp=temp->next;
         }
         else
         {
-            tempC->next=a;
+          temp->next=list2;
+          list2=list2->next;
+          temp=temp->next;
         }
-
-        return c->next;
+       }
+     if(list1==NULL) temp->next=list2;
+     else temp->next=list1;
+     return t->next;
     }
 };
