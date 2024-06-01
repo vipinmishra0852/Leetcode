aa5c941f3d0f03b98aad522c45c5efd0aacb88a7
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-01 09:31:41 +0530

    Time: 8 ms (16.57%) | Memory: 11.7 MB (34.06%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,52 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    
-    ListNode* getNode(ListNode* head,int idx )
-    {   ListNode* t=head;
-        for(int i=1;i<=idx;i++)
-        {
-           t=t->next;
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        
+        while (curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        return t;
-    }
-    ListNode* reverseList(ListNode* head)
-    { ios::sync_with_stdio(false);
-    cin.tie(NULL);
-     int n=0;
-     ListNode* temp=head;
-     while(temp)
-     {
-        temp=temp->next;
-        n++;
-     }
-     int i=0;
-     int j=n-1;
-     while(i<j)
-     {
-        ListNode* left=getNode(head,i);
-        ListNode* right=getNode(head,j);
-        int c=left->val;
-        left->val=right->val;
-        right->val=c;
-        i++;
-        j--;
-
-     }
-     temp=head;
-     while(temp)
-     {
-        temp=temp->next;
-     }
-     return head;   
+        
+        return prev;
     }
 };
