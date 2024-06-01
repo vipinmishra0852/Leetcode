26a2b23d83d9f64d30bb1d506b781eb4735a6b0c
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-01 09:32:16 +0530

    Time: 3 ms (78.63%) | Memory: 11.7 MB (15.75%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        ios::sync_with_stdio(false);
+
+        cin.tie(NULL);
         ListNode* prev = NULL;
         ListNode* curr = head;
         ListNode* next = NULL;
