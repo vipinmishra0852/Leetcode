c6fd58b78648c06414c0ed88a7278929939baba4
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-08 11:31:05 +0530

    Time: 3 ms (68.35%) | Memory: 12.6 MB (11.23%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& arr) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        int n=arr.size();
+        vector<int>ans(n,0);
+        ans[n-1]=arr[n-1];
+        stack<int>st;
+        st.push(arr[n-1]);
+
+        for(int i=n-2 ;i>=0;i--)
+        {
+            while(st.size()>0 && st.top()>arr[i])
+            {
+              st.pop();
+            }
+            if(st.size()==0) ans[i]=arr[i];
+            else ans[i]=arr[i]-st.top();
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
