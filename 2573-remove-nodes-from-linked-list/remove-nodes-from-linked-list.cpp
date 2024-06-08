c8b5274575dcfed8d80625d0c8764c5cc5a77d14
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-08 15:34:23 +0530

    Time: 259 ms (78.84%) | Memory: 189.8 MB (5.03%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -1,77 +1,65 @@
-class Solution
-{
+class Solution {
 public:
-  int get_size(ListNode *temp)
-  {
-    int n = 0;
-    while (temp != nullptr)
-    {
-      n++;
-      temp = temp->next;
-    }
-    return n;
-  }
-
-  ListNode *removeNodes(ListNode *head)
-  {
-    if (head == nullptr || head->next == nullptr)
-      return head;
-
-    ListNode *temp = head;
-    int n = get_size(temp);
-    temp = head;
-    vector<int> v;
-
-    while (temp != nullptr)
-    {
-      v.push_back(temp->val);
-      temp = temp->next;
+    int get_size(ListNode* temp) {
+        int n = 0;
+        while (temp != nullptr) {
+            n++;
+            temp = temp->next;
+        }
+        return n;
     }
 
-    vector<int> nge(n, -1);
-    stack<int> st;
-    st.push(v[n - 1]);
-    for (int i = n - 2; i >= 0; i--)
-    { // pop all the answer in nge array
-      while (st.size() > 0 && st.top() <= v[i])
-      {
-        st.pop();
-      }
-      if (st.size() == 0)
-        nge[i] = -1;
-      else
-      {
-        // push the element
-        nge[i] = st.top();
-      }
-      st.push(v[i]);
+    ListNode* removeNodes(ListNode* head) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode* temp = head;
+        int n = get_size(temp);
+        temp = head;
+        vector<int> v;
+
+        while (temp != nullptr) {
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        vector<int> nge(n, -1);
+        stack<int> st;
+        st.push(v[n - 1]);
+
+        for (int i = n - 2; i >= 0; i--) {
+            while (!st.empty() && st.top() <= v[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                nge[i] = st.top();
+            }
+            st.push(v[i]);
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (nge[i] == -1) {
+                ans.push_back(v[i]);
+            }
+        }
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* h = dummy;
+        temp = head;
+        int i = 0;
+
+        while (temp != nullptr && i < ans.size()) {
+            if (temp->val == ans[i]) {
+                h->next = new ListNode(temp->val);
+                h = h->next;
+                i++;
+            }
+            temp = temp->next;
+        }
+
+        return dummy->next;
     }
-
-    vector<int> ans;
-    for (int i = 0; i < n; i++)
-    {
-      if (nge[i] == -1)
-      {
-        ans.push_back(v[i]);
-      }
-    }
-
-    ListNode *dummy = new ListNode(-1);
-    ListNode *h = dummy;
-    temp = head;
-    int i = 0;
-
-    while (temp != nullptr && i < ans.size())
-    {
-      if (temp->val == ans[i])
-      {
-        h->next = new ListNode(temp->val);
-        h = h->next;
-        i++;
-      }
-      temp = temp->next;
-    }
-
-    return dummy->next;
-  }
 };
