17d1af4debab1500b6b4fa90cb6bd129dfc32205
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-10 19:05:24 +0530

    Time: 9 ms (36.51%) | Memory: 10.3 MB (45.40%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.cpp
@@ -0,0 +1,56 @@
+class Solution
+{
+public:
+  string predictPartyVictory(string s)
+  { ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    queue<int> q;
+    queue<int> d;
+    queue<int> r;
+    int n = s.length();
+    for (int i = 0; i < n; i++)
+    {
+      q.push(i);
+      if (s[i] == 'R')
+        r.push(i);
+      else
+        d.push(i);
+    }
+
+    while (q.size() > 1)
+    {
+      if (s[q.front()] == 'X')
+        q.pop();
+      else if (s[q.front()] == 'R')
+      {
+        if (d.size() == 0)
+          return "Radiant";
+        else
+        { 
+          s[d.front()]='X';
+          d.pop();
+          q.push(q.front());
+          q.pop();
+          r.push(r.front());
+          r.pop();
+        }
+      }
+      else
+      {
+         if (r.size() == 0)
+          return "Dire";
+        else
+        { s[r.front()]='X';
+          r.pop();
+          q.push(q.front());
+          q.pop();
+          d.push(d.front());
+          d.pop();
+      }
+    }
+
+    }
+    if(s[q.front()]=='R') return "Radiant";
+    else return "Dire";
+  }
+};
