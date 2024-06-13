61a4b025b67211bfb0379f1dc80610180ec46d3a
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-13 19:06:26 +0530

    Time: 7 ms (15.13%) | Memory: 14 MB (74.59%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -37,10 +37,19 @@ public:
         }
 
     }
+     void preOrder(TreeNode *root, int level, vector<int> &ans)
+  {
+    if (root == NULL)
+      return;
+    ans[level] = root->val;
+    preOrder(root->left, level + 1, ans);
+    preOrder(root->right, level + 1, ans);
+  }
     vector<int> rightSideView(TreeNode* root) 
     {
      vector<int>ans(levels(root),0);
-     levelOrder(root,ans);
+    //  levelOrder(root,ans); //Approach 1
+    preOrder(root,0,ans); //Approach 2
      return ans;   
     }
 };
