34f92d63612a034d6b60aa06ce840782e86327f6
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-15 01:05:13 +0530

    Time: 15 ms (9.62%) | Memory: 20.2 MB (29.36%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    long long maxi(TreeNode* root) {
-        if(root == nullptr) return LLONG_MIN; // Use LLONG_MIN for long long type
-        return max((long long)root->val, max(maxi(root->left), maxi(root->right)));
-    }
-
-    long long mini(TreeNode* root) {
-        if(root == nullptr) return LLONG_MAX; // Use LLONG_MAX for long long type
-        return min((long long)root->val, min(mini(root->left), mini(root->right)));
-    }
-
+   void inorder(TreeNode* root,vector<int>&ans)
+   {
+    if(root==NULL) return;
+    inorder(root->left,ans);
+    ans.push_back(root->val);
+    inorder(root->right,ans);
+    return;
+   }
     bool isValidBST(TreeNode* root) {
-        if(root == nullptr) return true;
-        if(root->left != nullptr && root->val <= maxi(root->left)) return false; // Correct the type casting
-        if(root->right != nullptr && root->val >= mini(root->right)) return false; // Correct the type casting
-        return isValidBST(root->left) && isValidBST(root->right);
+        vector<int>ans;
+        inorder(root,ans);
+        for(int i=1;i<ans.size();i++)
+        {
+            if(ans[i-1]>=ans[i]) return false;
+        }
+        return true;
     }
 };
