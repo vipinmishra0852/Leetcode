9cb86aa0fdeaa9453f9307cf3ce91679ddd5c365
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-15 14:51:44 +0530

    Time: 5 ms (16.33%) | Memory: 10.4 MB (6.05%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -4,22 +4,24 @@ public:
     void inorder(TreeNode* root,vector<int>&sortedVec)
     {
     if(root==NULL) return;
+     inorder(root->right,sortedVec);
+     sortedVec.push_back(root->val);
     inorder(root->left,sortedVec);
-    sortedVec.push_back(root->val);
-    inorder(root->right,sortedVec);
+    ;
+   
     }
 
      TreeNode* fixValue(TreeNode* root, const vector<int>& sortedVec, const vector<int>& sum, int& i) {
         if (root == NULL) return NULL;
 
-        fixValue(root->left, sortedVec, sum, i);
-
+      
+   fixValue(root->right, sortedVec, sum, i);
         if (sortedVec[i] == root->val) {
             root->val = sum[i];
             ++i;
         }
 
-        fixValue(root->right, sortedVec, sum, i);
+       fixValue(root->left, sortedVec, sum, i);
 
         return root;
     }
@@ -29,10 +31,11 @@ public:
     if(root==NULL) return NULL;
     vector<int>sortedVec;
     inorder(root,sortedVec);  
+    int n=sortedVec.size();
    vector<int> sum(sortedVec.size(), 0);
-        sum[sortedVec.size() - 1] = sortedVec[sortedVec.size() - 1];
-        for (int i = sortedVec.size() - 2; i >= 0; --i) {
-            sum[i] = sum[i + 1] + sortedVec[i];
+        sum[0] = sortedVec[0];
+        for (int i=1; i < n; i++) {
+            sum[i] = sum[i - 1] + sortedVec[i];
         }
         int i=0;
     fixValue(root,sortedVec,sum,i);
