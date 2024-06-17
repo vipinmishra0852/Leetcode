219f718f8927b92458092f2afcd798c7d019e352
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-17 20:12:19 +0530

    Time: 15 ms (6.94%) | Memory: 8.5 MB (83.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {
-            return false;
-        }
-        
-        unordered_map<char, int> m1, m2;
-        
-        // Count frequencies of characters in s and t
-        for (char ch : s) {
-            m1[ch]++;
-        }
-        
-        for (char ch : t) {
-            m2[ch]++;
-        }
-        
-        // Compare the frequency maps
-        return m1 == m2;
+        sort(s.begin(),s.end());
+         sort(t.begin(),t.end());
+         return (s==t);
     }
 };
