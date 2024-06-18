872c6c3a611d15771cd9f0550a9490aa1d89d34a
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-18 20:11:04 +0530

    Time: 3 ms (91.01%) | Memory: 13.3 MB (35.72%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,11 +1,24 @@
-
-class Solution
-{
+/*
+// Definition for a Node.
+class Node {
 public:
-  Node *copyRandomList(Node *head)
-  {
-    // Step 1 : Creating the deep copy without random pointer.
-    ios::sync_with_stdio(false);
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head)
+    {
+       ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     Node *dummy = new Node(100);
@@ -19,55 +32,28 @@ public:
       tempC = tempC->next;
       temp = temp->next;
     }
-    Node *duplicate = dummy->next;
-    // Step 2 : alternate connections
-    Node *a = head;
-    Node *b = duplicate;
-    dummy = new Node(-1);
-    Node *tempD = dummy;
-
-    while (a)
+    Node *b = dummy->next;
+    Node* a = head ;
+    unordered_map<Node*,Node*>m;
+    Node* tempA=a;
+    Node* tempB=b;
+    while(tempA!=NULL)
     {
-      tempD->next = a;
-      a = a->next;
-      tempD = tempD->next;
-      tempD->next = b;
-      b = b->next;
-      tempD = tempD->next;
+        m[tempA] = tempB;
+        tempA = tempA -> next;
+        tempB = tempB -> next;
     }
-    dummy = dummy->next;
-    //    Step : 3 Making Random COnnections
-
-    Node *t1 = dummy;
-    while (t1)
+    for(auto i:m)
     {
-      Node *t2 = t1->next; // t2 is for duplicate
-      t2 = t1->next; //
-      if (t1->random) t2->random = t1->random->next;
-      t1 = t1->next->next;
+        Node* o = i.first;
+        Node* d = i.second;
+        if(o->random!=NULL)
+        {
+           Node* oRandom = o->random;
+           Node* dRandom = m[oRandom] ; 
+           d->random = dRandom;
+        }
     }
-
-    // Step : 4 Removing the connections
-
-    Node *d1 = new Node(-1);
-    Node *d2 = new Node(-1);
-    t1 = d1;
-    Node *t2 = d2;
-
-    Node *t = dummy;
-    while (t)
-    {
-      t1->next = t;
-      t = t->next;
-      t1 = t1->next;
-      t2->next = t;
-      t = t->next;
-      t2 = t2->next;
+    return b;
     }
-    t1->next = NULL;
-    t2->next = NULL;
-    d1 = d1->next;
-    d2 = d2->next;
-    return d2;
-  }
 };
