50176b59fb22cec92645a6cd4b0c3973a9e1de75
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-19 18:58:13 +0530

    Time: 107 ms (28.24%) | Memory: 61 MB (64.33%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,50 +1,17 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    // Function to swap two elements
-    void swap(int &a, int &b) {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-
-    // Partition function for Quickselect
-    int partition(vector<int> &arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low;
-
-        for (int j = low; j < high; ++j) {
-            if (arr[j] > pivot) { // Note the comparison here for kth largest
-                swap(arr[i], arr[j]);
-                ++i;
-            }
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+      priority_queue<int,vector<int>,greater<int>>pq;
+      int n=nums.size();
+      for(int i=0;i<n;i++)
+      {
+        pq.push(nums[i]);
+        if(pq.size()>k)
+        {
+            pq.pop();
         }
-        swap(arr[i], arr[high]);
-        return i;
-    }
-
-    // Quickselect function
-    int quickSelect(vector<int> &arr, int low, int high, int k) {
-        if (low <= high) {
-            int pi = partition(arr, low, high);
-
-            if (pi == k) {
-                return arr[pi];
-            } else if (pi < k) {
-                return quickSelect(arr, pi + 1, high, k);
-            } else {
-                return quickSelect(arr, low, pi - 1, k);
-            }
-        }
-        return -1; // This should never be reached if k is valid
-    }
-
-    int findKthLargest(vector<int>& nums, int k) {
-        ios::sync_with_stdio(false);
-        cin.tie(NULL);
-        int n = nums.size();
-        return quickSelect(nums, 0, n - 1, k - 1);
+      }
+      return pq.top();
     }
 };
