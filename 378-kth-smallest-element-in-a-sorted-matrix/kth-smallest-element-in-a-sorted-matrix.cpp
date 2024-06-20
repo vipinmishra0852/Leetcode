ff3097ebeda2481d86ded12c85f7387ee0da3b89
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-20 16:33:17 +0530

    Time: 25 ms (58.64%) | Memory: 17.3 MB (57.48%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -4,15 +4,21 @@ public:
     {
      int row = matrix.size();
      int column = matrix[0].size();
+     int n = row*column;
      vector<int>me;
+     priority_queue<int,vector<int>,greater<int>>pq;
      for(int i=0;i<row;i++)
      {
         for(int j=0;j<column;j++)
+        {if(pq.size()>n-k)
         {
-         me.push_back(matrix[i][j]);
+            pq.pop();
+           
+        }
+          pq.push(matrix[i][j]);
         }
      }   
-     sort(me.begin(),me.end());
-     return me[k-1];
+    
+     return pq.top();
     }
 };
