ca9e0ce26c436c838314770a164804bfb4f4bbbe
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-06-27 17:35:01 +0530

    Time: 302 ms (18.61%) | Memory: 105.9 MB (15.31%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -11,30 +11,25 @@ public:
         vector<int>ele(2,0);
         ele[0] = points[0][0];
         ele[1] = points[0][1];
-        priority_queue<pair<int,int>>pq;
-         pq.push({ele[0],ele[1]});  
+   
         int i = 1;
        while(i < points.size() )
         {   
             vector<int>curr = points[i];
             if(ele[1] >= curr[0] )
             {    
-                
-                
-                pq.pop();
+    
                 ele[0] = max(curr[0],ele[0]);
                 ele[1] = min(curr[1],ele[1]);
-                pq.push({ele[0],ele[1]});  
-               
-                
+
             }
             else
             {
              count++;
-             pq.pop();
+           
              ele[0] = curr[0];
              ele[1] = curr[1];
-             pq.push({ele[0],ele[1]});  
+            
             
             }
            i++; 
