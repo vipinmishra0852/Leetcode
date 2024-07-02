d91247fb2553138007fd28bd6234d681e042d085
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-02 21:24:49 +0530

    Time: 97 ms (15.28%) | Memory: 101 MB (5.64%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,10 @@
 
 class Solution {
 public:
+    int dp[100005];
     int helper(vector<int>& prices, int i, int minPrice) {
         if (i == prices.size()) return 0;
-        
+        if(dp[i] != -1) return dp[i];
         // Calculate profit if we sell at the current price
         int profit = prices[i] - minPrice;
         
@@ -11,11 +12,12 @@ public:
         minPrice = min(minPrice, prices[i]);
         
         // Either sell at the current price or skip to the next day
-        return max(profit, helper(prices, i + 1, minPrice));
+        return dp[i] = max(profit, helper(prices, i + 1, minPrice));
     }
 
     int maxProfit(vector<int>& prices) {
         if (prices.empty()) return 0;
+        memset(dp,-1,sizeof dp);
         return helper(prices, 0, prices[0]);
     }
 };
