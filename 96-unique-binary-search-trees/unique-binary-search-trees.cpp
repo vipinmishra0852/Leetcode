fbf1cd50fb14c8d4900ef9735bae776e0052cc43
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-02 14:00:07 +0530

    Time: 2 ms (61.41%) | Memory: 7.1 MB (82.24%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -16,8 +16,18 @@ public:
     int numTrees(int n)
     {
     dp.clear();
-    dp.resize(30,-1);
-    int ans = helper(n);
-    return ans;
+    dp.resize(30,0);
+    // int ans = helper(n);
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3;i<=n ; i++)
+    {
+       for(int k = 1 ; k<=i;k++)
+       {
+       dp[i] += dp[k-1] * dp[i-k];
+       } 
+    }
+    return dp[n];
     }
 };
