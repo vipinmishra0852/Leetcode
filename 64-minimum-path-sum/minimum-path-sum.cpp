1979a6fac8965e3d93a15a8ade004304e8b0e8a5
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-02 12:35:52 +0530

    Time: 7 ms (60.85%) | Memory: 36.3 MB (13.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -16,12 +16,30 @@ public:
       m = grid[0].size();
      copygrid = grid;
      dp.clear();
-     dp.resize(250,vector<int>(250,-1));
+     dp.resize(250,vector<int>(250,0));
      if(m == 1 && n  == 1) return grid[m-1][n-1];
-     int i = 0;
-     int j = 0;
-     int newans = 0;
-     newans =  helper(0,0);
-    return ans;  
+    //  int i = 0;
+    //  int j = 0;
+    //  int newans = 0;
+    //  newans =  helper(0,0);
+     dp[n-1][m-1] = grid[n-1][m-1];
+     for(int i = n - 2 ; i >= 0 ; i--)
+     {
+        dp[i][m-1] = grid[i][m-1] + dp[i+1][m-1];
+     }
+      for(int i = m - 2 ; i >= 0 ; i--)
+     {
+        dp[n-1][i] = grid[n-1][i] + dp[n-1][i+1];
+     }
+     for(int  i = n-2 ; i>= 0 ; i--)
+     {
+        for(int j = m-2 ; j >= 0 ; j--)
+        {
+            dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1]);
+        }
+     }
+     
+
+    return dp[0][0];  
     }
 };
