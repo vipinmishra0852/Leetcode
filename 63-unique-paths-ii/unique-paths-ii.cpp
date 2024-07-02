49166f6c47a0cab25a9033b255b50ed007208ff9
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-02 10:54:33 +0530

    Time: 3 ms (53.65%) | Memory: 12.8 MB (8.47%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
-        int m = arr.size();
-        int n = arr[0].size();
-
-        // If the starting point has an obstacle, return 0 as there's no way to start.
-        if (arr[0][0] == 1) return 0;
-
-        // Initialize the dp array with 0s
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-
-        // Set the starting point
-        dp[0][0] = 1;
-
-        // Fill the dp array
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                // If there's an obstacle, skip the cell
-                if (arr[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    // If not in the first row, add paths from the top cell
-                    if (i > 0) dp[i][j] += dp[i - 1][j];
-                    // If not in the first column, add paths from the left cell
-                    if (j > 0) dp[i][j] += dp[i][j - 1];
-                }
-            }
-        }
-
-        return dp[m - 1][n - 1];
+    int n,m;
+    vector<vector<int>>grid,dp;
+    int f(int i,int j)
+    {   if(i < 0 || j < 0 || i >=n || j >= m) return 0;
+        if(grid[i][j] == 1) return 0;   
+        if( i == n-1 && j == m-1) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = f(i,j+1) + f(i+1,j); 
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+    {
+      grid = obstacleGrid;
+      n = grid.size();
+      m = grid[0].size();
+      dp.clear();
+      dp.resize(105,vector<int>(105,-1));
+      if(grid[n-1][m-1] == 1) return 0;
+      return f(0,0);  
     }
 };
