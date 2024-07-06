c3d831181982e45efd667a7c4c8dab1a67fa21b5
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-06 18:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.56%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    int dp[5005];
     vector<int>copy;
     int n;
     int helper(int idx,int &count)
     {
     if(idx == n-1) return 0;
+    if(dp[idx]!=-1) return dp[idx];
     if(copy[idx] - copy[idx-1] == copy[idx+1] - copy[idx]) 
         {
          count++;
@@ -19,10 +21,11 @@ public:
         }
         
         }
-        return helper(idx+1,count); 
+        return dp[idx] = helper(idx+1,count); 
     }
     int numberOfArithmeticSlices(vector<int>& nums) 
     {   n = nums.size();
+    memset(dp,-1,sizeof(dp));
     int count = 0;
        if(n<3) return 0; 
        int idx = 1;
