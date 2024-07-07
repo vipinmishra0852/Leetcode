e69d17488a836e89c5a71ce88c23d92cfd87cf78
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-07 19:49:22 +0530

    Time: 2 ms (53.84%) | Memory: 7.1 MB (72.30%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
      return    ( numBottles + ( numBottles - 1 ) / (numExchange-1) );
     }
 };
