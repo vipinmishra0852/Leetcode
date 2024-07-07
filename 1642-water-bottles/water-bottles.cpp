e034241d3d9cb01168a45495c5bcce433de1d698
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-07 19:48:37 +0530

    Time: 2 ms (53.84%) | Memory: 7.2 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
-    int numWaterBottles(int numBottles, int numExchange) 
-    {
-    int count = 0;
-    count = count + numBottles;
-    
-    while(numBottles >= numExchange)
-    {
-     int rem = numBottles % numExchange ;
-     int div = numBottles / numExchange ; 
-      count = count + div;
-      if(div + rem >= numExchange)
-      {
-       numBottles = div + rem;
-      }
-      else break;
-    }    
-    return count;
+    int numWaterBottles(int numBottles, int numExchange) {
+     return    ( numBottles + ( numBottles - 1 ) / (numExchange-1) );
     }
 };
