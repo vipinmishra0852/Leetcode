c3ff65139208a652545cd6367215d2e89c4aa2bd
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-11 00:23:12 +0530

    Time: 10 ms (7.41%) | Memory: 14.1 MB (37.81%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    int minOperations(vector<string>& logs) 
-    {
-    stack<string>st;
-    int n = logs.size();
-    int i = 0;
-    for(int i = 0; i<n ;i++)
-    {
-    if(logs[i] == "../")
-    {
-        if(st.size() == 0) continue;
-        else st.pop();
-    }
-    else if(logs[i] == "./") continue;
-    else
-    {
-        st.push(logs[i]);
-    }
-    
-    }
-    int ans = 0;
-    while(!st.empty())
-    {
-        st.pop();
-        ans++;
-    }
-    return ans;
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        int n = logs.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (logs[i] == "../") {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            } else if (logs[i] == "./") {
+                continue;
+            } else {
+                st.push(logs[i]);
+            }
+        }
+        
+        return st.size();
     }
 };
