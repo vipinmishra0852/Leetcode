665325a3442f4b7ffa162b1685dc838f108134f7
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-18 19:22:27 +0530

    Time: 73 ms (62.21%) | Memory: 102.6 MB (26.24%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int n,m;
-    void dfs(int i,int j,vector<vector<int>>& grid,int &peri)
+    int n,m,peri;
+    void dfs(int i,int j,vector<vector<int>>& grid)
     {
       if(i < 0 || j < 0 || i >= n || j >= m  || grid[i][j] == 0) 
       {
@@ -10,27 +10,26 @@ public:
       }
       if(grid[i][j] == -1) return;
       grid[i][j] = -1;
-      dfs(i+1,j,grid,peri);
-      dfs(i-1,j,grid,peri);
-      dfs(i,j-1,grid,peri);
-      dfs(i,j+1,grid,peri);
+      dfs(i+1,j,grid);
+      dfs(i-1,j,grid);
+      dfs(i,j-1,grid);
+      dfs(i,j+1,grid);
     }
     int islandPerimeter(vector<vector<int>>& grid) 
     {
      n = grid.size();
      m = grid[0].size();
-   int peri = 0;
+     peri = 0;
      for(int i = 0 ; i<n;i++)
      {
         for(int j = 0 ; j < m ;j++)
         {
          if(grid[i][j] == 1)
          {
-            peri++;
-            dfs(i,j,grid,peri);
+            dfs(i,j,grid);
          }
         }
      }   
-     return peri-1;
+     return peri;
     }
 };
