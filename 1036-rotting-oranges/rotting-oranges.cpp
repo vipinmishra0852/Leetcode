4fd0397965d7a4df4772ddca1eaec47747b89d40
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-07-19 15:18:09 +0530

    Time: 5 ms (58.10%) | Memory: 16.6 MB (13.16%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -17,33 +17,46 @@ public:
                 }
             }
         }
-
+          q.push({-1,-1});
         if (fresh == 0) return 0; // No fresh oranges to rot
-
-        int minutes = -1; // Start at -1 to offset the last increment after the last fresh orange rots
+        
+        int minutes = 0; 
         vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         // BFS to simulate the rotting process
-        while (!q.empty()) {
-            int size = q.size();
-            minutes++;
-            for (int i = 0; i < size; i++) {
-                auto [x, y] = q.front();
-                q.pop();
-
-                for (auto& dir : directions) {
-                    int newX = x + dir[0];
-                    int newY = y + dir[1];
-
-                    if (newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == 1) {
-                        grid[newX][newY] = 2;
-                        q.push({newX, newY});
-                        fresh--;
-                    }
+        while (!q.empty()) 
+        {
+            auto cell = q.front();
+            q.pop();
+            if(cell.first == -1 && cell.second == -1)
+            {
+                minutes++;
+                if(!q.empty())
+                {
+                    q.push({-1,-1});
                 }
+                else {break;}
             }
+            else
+            {
+              int i = cell.first;
+              int j = cell.second;
+              for(int  d = 0 ; d < 4 ;d++)
+              {
+                int nr = i + directions[d][0];
+                int nc = j + directions[d][1];
+                if(nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
+                if(grid[nr][nc] == 2 || grid[nr][nc] == 0) continue;
+                fresh--;
+                grid[nr][nc] = 2;
+                q.push({nr,nc});
+
+              }
+
+            }
+            
         }
 
-        return fresh == 0 ? minutes : -1;
+        return fresh == 0 ? minutes-1 : -1;
     }
 };
