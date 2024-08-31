daf97836c6a3daea0cd3f1c08d9f17aa3c9be415
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-08-31 16:06:03 +0530

    Time: 12 ms (26.15%) | Memory: 27.3 MB (24.29%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -3,23 +3,23 @@ public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) 
     {
     int m = multiplier;
-    for(int i=0 ;i<k;i++)
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>min_heap;
+    for(int i = 0;i<nums.size();i++)
     {
-     vector<int>temp = nums;
-     sort(temp.begin(),temp.end());
-     int n = temp[0];
-     temp[0] = temp[0]*m;
-     int m = temp[0];
-     for(int j = 0 ; j < nums.size();j++)
-    {
-        if(nums[j] == n) 
-        {
-            nums[j] = m;
-            break;
-        }
-        
+      min_heap.push({nums[i],i});
     }
-    }    
+    while(k)
+    {
+        pair<int,int>p = min_heap.top();
+        int ele = p.first;
+        int idx = p.second;
+        min_heap.pop();
+        ele = ele*m;
+        nums[idx] = ele;
+        min_heap.push({ele,idx});
+        k--;
+    }
+    
     return nums;
     }
 };
