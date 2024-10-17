15587a642577e971e3903812e3348f673f90629c
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-10-17 23:38:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (12.29%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    vector<int>v;
-    
+    vector<int> v;
+
+    // Function to manually sort the vector in descending order using Bubble Sort
+    void manualSort() {
+        int n = v.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                // Compare adjacent elements and swap if they are in the wrong order
+                if (v[j] < v[j + 1]) {
+                    swap(v[j], v[j + 1]);
+                }
+            }
+        }
+    }
+
     void digits(int x) {
         while (x > 0) {
             int d = x % 10;
             x = x / 10;
             v.push_back(d);
         }
-        sort(v.begin(), v.end(), greater<int>());
+        // Call the custom sort function
+        manualSort();
     }
 
     int maximumSwap(int num) {
