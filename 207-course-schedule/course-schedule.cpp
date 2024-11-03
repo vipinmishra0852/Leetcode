0063efc57327e94d8624c90fd8550665031bef9e
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-11-03 21:39:30 +0530

    Time: 6 ms (58.19%) | Memory: 19.5 MB (37.60%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        // Initialize the graph as an adjacency list
-        vector<vector<int>> graph(numCourses);
-        vector<int> indegree(numCourses, 0);
-
-        // Build the graph and calculate indegrees
-        for (const auto& pre : prerequisites) {
-            int a = pre[0]; // Course to be taken
-            int b = pre[1]; // Prerequisite course
-            graph[b].push_back(a); // b must be taken before a
-            indegree[a]++; // Increase indegree of course a
-        }
-
-        // Queue for courses with no prerequisites
-        queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        int count = 0; // Count of courses that can be finished
-        while (!q.empty()) {
-            int n = q.front();
-            q.pop();
-            count++; // A course is finished
-
-            // Decrease indegree for all neighbors
-            for (int neighbour : graph[n]) {
-                indegree[neighbour]--;
-                if (indegree[neighbour] == 0) {
-                    q.push(neighbour); // If indegree is 0, add to queue
-                }
-            }
-        }
-
-        // If count equals number of courses, return true
-        return count == numCourses;
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+    int v = numCourses;
+    vector<vector<int>>graph(numCourses);    
+    vector<int>indegree(v,0);
+    for(auto pre:prerequisites)
+    {
+   int a = pre[0];
+   int b = pre[1];
+   graph[b].push_back(a);
+   indegree[a]++;
     }
+
+    queue<int>q;
+    for(int i=0;i<v;i++)
+    {
+        if(indegree[i] == 0)
+        { 
+            q.push(i);
+        }
+    }
+    int count = 0;
+    while(!q.empty())
+    {
+        int n = q.front();
+        q.pop();
+        count++;
+        
+        for(auto neighbour:graph[n])
+        {
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0)
+                {
+                    q.push(neighbour);
+                   
+                }
+         }
+
+    }
+    
+    if(count == v) return true;
+    
+    return false;
+}
+    
 };
