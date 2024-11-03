754211efef1e7f9a8cf819794ef237ccf9714d2a
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-11-03 21:25:30 +0530

    Time: 2 ms (83.69%) | Memory: 18.6 MB (70.69%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // Initialize the graph as an adjacency list
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        // Build the graph and calculate indegrees
+        for (const auto& pre : prerequisites) {
+            int a = pre[0]; // Course to be taken
+            int b = pre[1]; // Prerequisite course
+            graph[b].push_back(a); // b must be taken before a
+            indegree[a]++; // Increase indegree of course a
+        }
+
+        // Queue for courses with no prerequisites
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int count = 0; // Count of courses that can be finished
+        while (!q.empty()) {
+            int n = q.front();
+            q.pop();
+            count++; // A course is finished
+
+            // Decrease indegree for all neighbors
+            for (int neighbour : graph[n]) {
+                indegree[neighbour]--;
+                if (indegree[neighbour] == 0) {
+                    q.push(neighbour); // If indegree is 0, add to queue
+                }
+            }
+        }
+
+        // If count equals number of courses, return true
+        return count == numCourses;
+    }
+};
