2ea72ed8f44b694d7b79be7cc0f9781e0e9c4895
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-12-13 00:08:28 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (9.22%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        ios::sync_with_stdio(false);
-        cin.tie(NULL);
-        int n=nums.size();
-        int sum=0;
-        int i=0;
-        int j=0;
-         int minlength=INT_MAX;
-         int length=0;
-        while(j<n)
+    int minSubArrayLen(int k, vector<int>& nums) 
+    {
+    int n = nums.size();
+    int len = INT_MAX;
+    int i=0;
+    int j=0;
+    int sum = 0;
+    while(i < n)
+    { 
+        sum = sum + nums[i];
+        while(sum >= k)
         {
-         sum+=nums[j];
-          while(sum>=target){
-             length=j-i+1;
-             minlength = min(minlength,length);
-             sum-=nums[i];
-             i++;
-          }
-          j++;
-
+            
+        len=min(len,i-j+1);
+        sum = sum - nums[j];
+        j++;   
         }
-        if(minlength==INT_MAX){
-            return 0;
-        }
-        return minlength;
+      i++;  
         
+    }
+
+/*
+    for(int i =0;i<n;i++)
+    {
+        int sum=0;
+        for(int j=i;j<n;j++)
+        {
+        sum = sum+nums[j];
+        if(sum >= k)
+        {
+        len = min(len,j-i+1);
+        break;
+        }
+        }
+    } */
+    return (len == INT_MAX)?0:len;    
     }
 };
