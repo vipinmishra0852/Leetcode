48d0dd5768acb166b88e5d4b7931d653c740a925
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-12-13 10:42:51 +0530

    Time: 31 ms (59.61%) | Memory: 139 MB (43.48%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k)
-    { ios::sync_with_stdio(false);
-    cin.tie(NULL);
-     int n=arr.size();
-     if(k==1) return arr;
-      vector<int> ngi(n);
-        stack<int> st;
-        
-        ngi[n-1] = n;
-        st.push(n-1);
-        for (int i = n-2; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] <= arr[i])
-            { st.pop(); }
-            if (st.empty()) ngi[i] = n;
-            else ngi[i] = st.top();
-            st.push(i);
-        }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(k==1) return nums;
+        deque<int>dq;
         vector<int>ans;
-        int j=0;
-        for(int i=0;i<n-k+1;i++)
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            if(j<i) j=i;
-            int mx=arr[j]; // starting of the window
-            while(j<i+k) // means if nge is inside
-            {
-                mx=arr[j];
-                if(ngi[j]>=i+k) break;
-                j=ngi[j];
-            }
-            ans.push_back(mx);
+            while(dq.size() && nums[i]>nums[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+            int j=i-(k-1); // start of the window
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
         return ans;
-     
     }
 };
