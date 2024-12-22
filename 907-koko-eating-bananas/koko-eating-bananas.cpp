575b2d71a827da33f5a62113f55f6d12ce8072b7
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2024-12-22 20:07:48 +0530

    Time: 10 ms (51.40%) | Memory: 22.9 MB (21.92%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool check(int mid, vector<int>& piles, int h) {
-        long long count = 0; // Use long long to avoid overflow
-        int n = piles.size();
-        for (int i = 0; i < n; i++) {
-            if (mid >= piles[i]) {
-                count++;
-            } else if (piles[i] % mid == 0) {
-                count += piles[i] / mid;
-            } else {
-                count += piles[i] / mid + 1;
-            }
+    #define ll long long
+    ll check(int mid,vector<int>&nums)
+    {
+        ll sum = 0;
+        for(int i = 0; i<nums.size();i++)
+        {
+            if(nums[i] % mid == 0) {sum = sum + nums[i]/mid;}
+            else {sum = sum + ((nums[i]/mid) + 1);}
         }
-        return count <= h;
+        return sum;
     }
-
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        int max = *max_element(piles.begin(), piles.end());
-        int lo = 1;
-        int hi = max;
-        int ans = 0;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (check(mid, piles, h)) {
-                ans = mid;
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
+    int minEatingSpeed(vector<int>& nums, int h) 
+    {
+     sort(nums.begin(),nums.end());
+     ll lo = 1;
+     ll hi = nums[nums.size()-1];
+     ll ans = hi;
+     while(lo <= hi)
+     {
+       ll mid = lo + ((hi-lo)/2);
+        if(check(mid,nums) <= h)
+        {
+            ans = mid;
+            hi = mid - 1;
         }
-        return ans;
+        else lo = mid + 1;
+     }
+     return (int)ans;
     }
 };
