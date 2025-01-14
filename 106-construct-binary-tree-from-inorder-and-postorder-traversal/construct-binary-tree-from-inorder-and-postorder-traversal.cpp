a85effe38de75129b45800dc0199e91fbcf25d0b
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2025-01-14 23:07:00 +0530

    Time: 71 ms (5.33%) | Memory: 222.2 MB (5.01%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,26 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* build(vector<int>& in, int inLo, int inHi, vector<int>& post, int postLo, int postHi) {
-        if (inLo > inHi) return nullptr;
-        TreeNode* root = new TreeNode(post[postHi]);
-        if (inLo == inHi) return root;
 
-        int i = inLo;
-        while (i < inHi) {
-            if (in[i] == post[postHi]) break;
-            i++;
+    TreeNode* solve(vector<int> inorder,vector<int>postorder,int start,int end,int &idx)
+    {
+        if(start > end) return NULL;
+        int root_val = postorder[idx];
+        int i = start;
+        for(;i <= end ;i++)
+        {
+            if(root_val == inorder[i]) break;
+            
         }
-
-        int leftcount = i - inLo;
-        int rightcount = inHi - i;
-
-        root->left = build(in, inLo, i - 1, post, postLo, postLo + leftcount - 1);
-        root->right = build(in, i + 1, inHi, post, postLo + leftcount, postHi - 1);
+        idx--;
+        TreeNode* root = new TreeNode(root_val);
+        root->right = solve(inorder,postorder,i+1,end,idx);
+        root->left = solve(inorder,postorder,start ,i-1,idx);
+     
         return root;
     }
 
-    TreeNode* buildTree(vector<int>& in, vector<int>& post) {
-        int n = in.size();
-        return build(in, 0, n - 1, post, 0, n - 1);
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) 
+    {
+    int n = inorder.size();
+    int idx = n-1;
+    return solve(inorder,postorder,0,n-1,idx); 
     }
 };
