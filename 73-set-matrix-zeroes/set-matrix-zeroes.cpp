0651110256629e4ccde7082c6e0e68a43e05152a
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2025-02-17 10:29:50 +0530

    Time: 5 ms (3.78%) | Memory: 18.9 MB (12.91%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,69 +1,29 @@
 class Solution {
 public:
-   void setZeroes(vector<vector<int>>& matrix) {
-    int m = matrix.size();
-    int n = matrix[0].size();
     
-    bool firstRowZero = false;
-    bool firstColZero = false;
-    
-    // Check if the first row should be zeroed
-    for (int j = 0; j < n; j++) {
-        if (matrix[0][j] == 0) {
-            firstRowZero = true;
-            break;
-        }
-    }
-    
-    // Check if the first column should be zeroed
-    for (int i = 0; i < m; i++) {
-        if (matrix[i][0] == 0) {
-            firstColZero = true;
-            break;
-        }
-    }
-    
-    // Use the first row and first column to mark zeros
-    for (int i = 1; i < m; i++) {
-        for (int j = 1; j < n; j++) {
-            if (matrix[i][j] == 0) {
-                matrix[i][0] = 0; // Mark the corresponding row
-                matrix[0][j] = 0; // Mark the corresponding column
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+    set<int>s1;
+    set<int>s2;
+    int row = matrix.size();
+    int col = matrix[0].size();
+    for(int i = 0 ; i < row ; i++)
+    {
+        for(int j = 0 ; j < col ; j++)
+        {
+            if(matrix[i][j] == 0) {
+                s1.insert(i);
+                s2.insert(j);
             }
         }
     }
-    
-    // Set entire rows to zero based on first column marks
-    for (int i = 1; i < m; i++) {
-        if (matrix[i][0] == 0) {
-            for (int j = 1; j < n; j++) {
-                matrix[i][j] = 0;
-            }
+    for(int i = 0 ; i < row ; i++)
+    {
+        for(int j = 0; j < col ; j++)
+        {
+            if( s1.find(i) != s1.end() || s2.find(j) != s2.end() ) matrix[i][j] = 0;
         }
     }
     
-    // Set entire columns to zero based on first row marks
-    for (int j = 1; j < n; j++) {
-        if (matrix[0][j] == 0) {
-            for (int i = 1; i < m; i++) {
-                matrix[i][j] = 0;
-            }
-        }
     }
-    
-    // Set first row to zero if needed
-    if (firstRowZero) {
-        for (int j = 0; j < n; j++) {
-            matrix[0][j] = 0;
-        }
-    }
-    
-    // Set first column to zero if needed
-    if (firstColZero) {
-        for (int i = 0; i < m; i++) {
-            matrix[i][0] = 0;
-        }
-    }
-}
-
 };
