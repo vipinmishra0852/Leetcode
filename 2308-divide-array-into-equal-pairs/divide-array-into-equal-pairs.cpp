9585bd395d915070d0ac82fe70b83a0955b83cde
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2025-03-17 18:34:41 +0530

    Time: 1 ms (69.22%) | Memory: 18.6 MB (9.58%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     bool divideArray(vector<int>& nums) 
     {
+     ios::sync_with_stdio(0);
+     cin.tie(0);
+     cout.tie(0);
      map<int,int>mp;
      for(int ele:nums)
      {
