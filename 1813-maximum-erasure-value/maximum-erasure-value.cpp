b06fb0fb484c1e5e428e62dfc0554b48f4525069
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2025-07-22 19:49:24 +0530

    Time: 195 ms (33.89%) | Memory: 131.1 MB (31.90%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    int maximumUniqueSubarray(vector<int>& arr)
-    {
-     int score = 0;
-     map<int,int>mp;
-     set<int>st;
-     int n = arr.size();
-     int i = 0 ;
-     int j = 0;  
-     int sum = 0; 
-     
-     while( j < n)
-     {
-       if(  st.find(arr[j]) == st.end() ) // jb na mila
-       {
-        sum += arr[j];
-        score = max(score,sum);
-        st.insert(arr[j]);
-        mp[arr[j]] = j;
-        j++;
-       }
-       else{ // jb mil gya
-       
-        int idx = mp[arr[j]];
-        while( i<= idx)
-        {
-            sum-=arr[i];
-            st.erase(arr[i]);
-            i++;
+    int maximumUniqueSubarray(vector<int>& nums) {
+        unordered_set<int> seen;
+        int i = 0, j = 0, n = nums.size();
+        int sum = 0, maxSum = 0;
+        
+        while (j < n) {
+            if (seen.find(nums[j]) == seen.end()) {
+                seen.insert(nums[j]);
+                sum += nums[j];
+                maxSum = max(maxSum, sum);
+                j++;
+            } else {
+                seen.erase(nums[i]);
+                sum -= nums[i];
+                i++;
+            }
         }
-       }
-     }
-     return score;
+        
+        return maxSum;
     }
 };
