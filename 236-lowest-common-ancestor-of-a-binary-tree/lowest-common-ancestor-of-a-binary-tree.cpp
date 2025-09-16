3db1d45a077479934cacfa2d4f792c09217e8123
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2025-09-17 00:31:37 +0530

    Time: 9 ms (78.37%) | Memory: 19 MB (5.27%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,18 +9,28 @@
  */
 class Solution {
 public:
+    bool getPath(TreeNode* root, TreeNode* node,vector<TreeNode*>&vec)
+    {
+        if(root == NULL) return false;
+        vec.push_back(root);
+        if(root == node) return true;
+        if( (getPath(root->left,node,vec)) || (getPath(root->right,node,vec)) ) return true;
+        vec.pop_back();
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-    if(root == NULL) return NULL;
-    if(root == p || root == q) return root;
-    TreeNode* leftN = lowestCommonAncestor(root->left,p,q);
-    TreeNode* rightN = lowestCommonAncestor(root->right,p,q);
-
-    if(leftN != NULL && rightN != NULL)
-    return root;
-    if(leftN != NULL)
-    return leftN;
-    
-    return rightN;    
+    vector<TreeNode*>pvec;
+    vector<TreeNode*>qvec;
+    getPath(root,p,pvec);
+    getPath(root,q,qvec); 
+    int i = 0 ;
+    TreeNode* lca = NULL;
+    while( i < pvec.size() && i < qvec.size() && pvec[i] == qvec[i] )   
+    {
+     lca = pvec[i];
+     i++;
+    }
+    return lca;
     }
 };
