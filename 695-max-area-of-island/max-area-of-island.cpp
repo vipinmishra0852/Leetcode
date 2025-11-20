e7b0d035e57b8c8b22a6f3ce2306ed46749f569e
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2025-11-20 23:02:35 +0530

    Time: 2 ms (60.33%) | Memory: 27.5 MB (81.01%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -4,7 +4,7 @@ public:
     int dfs(int i,int j,vector<vector<int>>& grid)
     {
         if(i < 0 || j < 0 || i >= n || j >= m  || grid[i][j] != 1) return 0;
-        grid[i][j] = -1;
+        grid[i][j] = 0;
         return 1 + dfs(i+1,j,grid) +  dfs(i-1,j,grid) + dfs(i,j+1,grid) + dfs(i,j-1,grid); 
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
