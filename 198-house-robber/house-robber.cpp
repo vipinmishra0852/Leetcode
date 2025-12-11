556235d41c53d87ea4476aecd6df327caecc91cc
vipinmishra0852 <125123774+vipinmishra0852@users.noreply.github.com>
2025-12-11 20:30:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (62.30%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,17 +5,26 @@ public:
     vector<int>temp;
     int helper(int idx)
     {
-        if(idx >=n ) return 0;
+        if(idx < 0 ) return 0;
         if(dp[idx]!=-1) return dp[idx];
-        int pick = temp[idx] + helper(idx+2);
-        int not_pick = helper(idx+1);
+        int pick = temp[idx] + helper(idx-2);
+        int not_pick = helper(idx-1);
         return dp[idx]=max(pick,not_pick);
     }
     int rob(vector<int>& nums) {
     n = nums.size();
     dp.resize(n,-1);
-    temp = nums;
-    helper(0);
-    return dp[0]; 
+    // temp = nums;
+    // helper(n-1);
+    dp[0] = nums[0];
+    for(int idx = 1 ; idx < n ; idx++)
+    {
+        int pick = nums[idx];
+        if( idx-2 >= 0 ) pick+=dp[idx-2];
+        int not_pick = dp[idx-1];
+        dp[idx] = max(pick,not_pick);
+    }
+
+    return dp[n-1]; 
     }
 };
